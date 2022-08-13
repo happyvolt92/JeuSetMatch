a7413ad8248976d3b7f7569e51abaf6356396098
HappyVolt <elodiegage@gmail.com>
2022-08-13 09:26:29 +0200

    Match Test done

--- JeuSetMatchTests/MatchTestCase.swift
+++ JeuSetMatchTests/MatchTestCase.swift
@@ -5,20 +5,75 @@
 //  Created by Elodie Gage on 13/08/2022.
 //  Copyright © 2022 OpenClassrooms. All rights reserved.
 //
-
 import XCTest
+@testable import JeuSetMatch
 
 class MatchTestCase: XCTestCase {
-    func testExemple() {
-        XCTAssert(true)
+    // MARK: - Properties
+    var match: Match!
+
+    // MARK: - Setup
+
+    override func setUp() {
+        super.setUp()
+        match = Match()
     }
-    func testExemple2() {
-        XCTAssert(2*2 == 4)
+
+    // MARK: - Helpers
+
+    private func createSet(wonByPlayer player: Player) -> JeuSetMatch.Set {
+        let set = Set()
+        createManyGames(6, wonByPlayer: player, inSet: set)
+        return set
     }
-    func testExemple3() {
-        XCTAssert(true && false)
+
+    private func createManyGames(_ count: Int, wonByPlayer player: Player, inSet set: JeuSetMatch.Set) {
+        for _ in 1...count {
+            let game = Game()
+            game.winner = player
+            set.games.append(game)
+        }
     }
-    func testExemple4() {
-        XCTAssert(365%7 == 0)
+
+    private func createManySets(_ count: Int, wonByPlayer player: Player) {
+        for _ in 1...count {
+            let set = createSet(wonByPlayer: player)
+            match.sets.append(set)
+        }
+    }
+
+    // MARK: - Tests
+    func testGivenPlayerOneHasWonThreeSets_WhenGettingWinner_ThenWinnerShouldBePlayerOneAndGameIsOver() {
+        createManySets(3, wonByPlayer: .one)
+
+        XCTAssertEqual(match.winner, .one)
+        XCTAssertEqual(match.isOver, true)
+    }
+
+    func testGivenCurrentGameScoreIsNull_WhenPointIsAdded_ScoreIsOnlyIncremented() {
+        match.pointEnded(wonBy: .one)
+
+        XCTAssertEqual(match.sets.count, 1)
+        XCTAssertEqual(match.sets.last!.games.count, 1)
+        XCTAssertEqual(match.currentGame.scores[.one]!, 15)
+    }
+
+    func testGivenCurrentGameScoreIsForty_WhenPointIsAdded_GameIsEndedAndNewGameIsCreated() {
+        match.currentGame.scores[.one] = 40
+        match.pointEnded(wonBy: .one)
+
+        XCTAssertTrue(match.sets.last!.games.first!.isOver)
+        XCTAssertEqual(match.sets.last?.games.count, 2)
+    }
+
+    func testGivenCurrentGameScoreIsFortyAndSetScoreIs5_WhenPointIsAdded_SetIsEndedAndNewSetIsCreated() {
+        createManyGames(5, wonByPlayer: .one, inSet: match.sets.last!)
+        match.sets.last?.games.append(Game())
+        match.currentGame.scores[.one] = 40
+
+        match.pointEnded(wonBy: .one)
+
+        XCTAssertTrue(match.sets.first!.games.last!.isOver)
+        XCTAssertEqual(match.sets.count, 2)
     }
 }
