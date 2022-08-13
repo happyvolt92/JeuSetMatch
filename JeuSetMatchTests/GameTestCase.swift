afb21c6e89bc1e6f61a7218f3811c948ba3891aa
HappyVolt <elodiegage@gmail.com>
2022-08-13 09:18:14 +0200

    Test Game class done

--- JeuSetMatchTests/GameTestCase.swift
+++ JeuSetMatchTests/GameTestCase.swift
@@ -7,21 +7,54 @@
 //
 
 import XCTest
+@testable import JeuSetMatch
+
 
 class GameTestCase: XCTestCase {
-    func testExemple () {
-        XCTAssert(true)
+    var game: Game!
+    
+    override func setUp() {
+        super.setUp()
+        game = Game()
     }
-    func testExemple() {
-        XCTAssert(false)
+    func setPlayerOneScore(_ score: Int) {
+        game.scores[.one] = score
     }
-    func testExemple2() {
-        XCTAssert(2*2 == 4)
+    
+    func testGivenScoreIsNull_WhenIncrementingPlayer1Score_ThenScoreShouldBeFifteen() {
+        // 1 GIVEN
+      
+
+        // 2 WHEN
+        game.incrementScore(forPlayer: .one)
+
+        // 3 THEN
+        XCTAssert(game.scores[.one]! == 15)
+        XCTAssert(game.scores[.two]! == 0)
     }
-    func testExemple3() {
-        XCTAssert(true && false)
+    func testGivenScoreIsFifteen_WhenIncrementingPlayer1Score_ThenScoreShouldBeThirty() {
+        setPlayerOneScore(15) // Pour le deuxième test
+        
+
+        game.incrementScore(forPlayer: .one)
+
+        XCTAssert(game.scores[.one]! == 30)
+        XCTAssert(game.scores[.two]! == 0)
     }
-    func testExemple4() {
-        XCTAssert(365%7 == 0)
+    func testGivenScoreIsThirty_WhenIncrementingPlayer1Score_ThenScoreShouldBeForty() {
+        setPlayerOneScore(30) // Pour le troisième test
+
+        game.incrementScore(forPlayer: .one)
+
+        XCTAssert(game.scores[.one]! == 40)
+        XCTAssert(game.scores[.two]! == 0)
+    }
+    func testGivenScoreIsForty_WhenIncrementingPlayer1Score_ThenGameIsOverAndWonByPlayer1() {
+        setPlayerOneScore(40)
+
+        game.incrementScore(forPlayer: .one)
+
+        XCTAssertEqual(game.winner, .one)
+        XCTAssertTrue(game.isOver)
     }
 }
