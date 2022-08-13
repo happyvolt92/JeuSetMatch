3521cffe25e07423f3043cb01ac6b0da4f904408
HappyVolt <elodiegage@gmail.com>
2022-08-13 09:04:50 +0200

    first Unit test

---
+++ JeuSetMatchTests/MatchTestCase.swift
@@ -0,0 +1,24 @@
+//
+//  MatchTestCase.swift
+//  JeuSetMatchTests
+//
+//  Created by Elodie Gage on 13/08/2022.
+//  Copyright © 2022 OpenClassrooms. All rights reserved.
+//
+
+import XCTest
+
+class MatchTestCase: XCTestCase {
+    func testExemple() {
+        XCTAssert(true)
+    }
+    func testExemple2() {
+        XCTAssert(2*2 == 4)
+    }
+    func testExemple3() {
+        XCTAssert(true && false)
+    }
+    func testExemple4() {
+        XCTAssert(365%7 == 0)
+    }
+}
---
+++ JeuSetMatchTests/SetTestCase.swift
@@ -0,0 +1,27 @@
+//
+//  SetTestCase.swift
+//  JeuSetMatchTests
+//
+//  Created by Elodie Gage on 13/08/2022.
+//  Copyright © 2022 OpenClassrooms. All rights reserved.
+//
+
+import XCTest
+
+class SetTestCase: XCTestCase {
+    func testExemple () {
+        XCTAssert(true)
+    }
+    func testExemple() {
+        XCTAssert(false)
+    }
+    func testExemple2() {
+        XCTAssert(2*2 == 4)
+    }
+    func testExemple3() {
+        XCTAssert(true && false)
+    }
+    func testExemple4() {
+        XCTAssert(365%7 == 0)
+    }
+}
